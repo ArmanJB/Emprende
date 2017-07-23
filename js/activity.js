33f35c1e02f2284d45fc54b3937941808db073a0
Armando Bustos <armanjbustos596@gmail.com>
2017-07-23 11:13:27 -0600

    Jugar->conocedor: img, drag and drop

--- js/activity.js
+++ js/activity.js
@@ -4,20 +4,35 @@ define(function (require) {
     var activity = require("sugar-web/activity/activity");
     var datastore = require("sugar-web/datastore");
     var jquery = require("jquery");
+    var interact = require("interact");
 
     //matrices
     var explorar = require("../js/explorar.js");
+    var jugar = require("../js/jugar.js");
 
     //variables de juego
     var gender = null;
     var name = null;
+    var healthC = null;
 
     //matrices de juego
     var matrixE = null;
     var matrixT = null;
     var matrixP = null;
+    var matrixC = null;
+    var matrixGameC = null;
 
-    //funcionaes de juego
+    //funcion drag
+    function dragMoveListener (event) {
+      var target = event.target, x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx, y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    }
+
+    //***funcionaes de juego***
+    //Emprendedores
     function initEmprendedores(){
       matrixE = matrixExplorar(1);
       gameE();
@@ -36,6 +51,7 @@ define(function (require) {
       $('#img_emprendedores').css('background-image', 'url('+gameE.img+')');
     }
 
+    //Tips
     function initTips(){
       matrixT = matrixExplorar(2);
       gameT();
@@ -54,6 +70,7 @@ define(function (require) {
       $('#img_tips').css('background-image', 'url('+gameT.img+')');
     }
 
+    //Pasos
     function initPasos(){
       matrixP = matrixExplorar(3);
       $('.step').each(function(){
@@ -65,6 +82,41 @@ define(function (require) {
             }
           });
       });
+    }
+
+    //Conocedor
+    function initConocedor(){
+      matrixC = matrixJugar(1);
+      healthC = 5;
+      matrixGameC = [];
+      $('#answ_area').empty();
+      for (var i = 1; i <= healthC ; i++) {
+        $('#health_conocedor').append('<div id="attempt'+i+'" class="attempt">'+i+'</div>');
+      }
+      gameC();
+    }
+
+    function gameC(){
+      if (matrixC.length == 0) {
+        $('.back_menu_jugar').click();
+        return;
+      }
+      var matrixAux = [];
+      for (var i = 0; i < 4; i++) {
+        var pos = Math.floor(Math.random()*matrixC.length);
+        matrixGameC.push(matrixC[pos]);
+        matrixAux.push(matrixC[pos]);
+        matrixC.splice(pos,1);
+        //
+        $('#item_name'+(i+1)).html(matrixGameC[i].name);
+        $('#item_detail'+(i+1)).html(matrixGameC[i].detail);
+      }
+      for (var i = 0; i < 4; i++) {
+        var pos = Math.floor(Math.random()*matrixAux.length);
+        $('#answ_area').append('<div id="drag'+(i+1)+'" class="drag"></div>');
+        $('#drag'+(i+1)).css('background-image', 'url('+matrixAux[pos].img+')');
+        matrixAux.splice(pos,1);
+      }
     }
 
     require(['domReady!'], function (doc) {
@@ -149,6 +201,8 @@ define(function (require) {
       $('#to_conocedor').on('click', function(){
         $('#menu_jugar').toggle();
         $('#conocedor').toggle();
+        //
+        initConocedor();
       });
 
       $('#to_pensador').on('click', function(){
@@ -229,6 +283,87 @@ define(function (require) {
         $('#modal').removeClass('hidden');
         $('#modal_content').removeClass('hidden');
       });
+
+      //Interact elements
+      interact('.drop').dropzone({
+        accept: '.drag',
+        overlap: 0.9,
+
+        ondropactivate: function (event) {
+          event.target.classList.add('drop-active'); //dropzone
+        },
+        ondragenter: function (event) {
+          var draggableElement = event.relatedTarget, dropzoneElement = event.target; //element in the dropzone
+          dropzoneElement.classList.add('drop-target');
+          draggableElement.classList.add('can-drop');
+        },
+        ondragleave: function (event) {
+          // remove the drop feedback style
+          event.target.classList.remove('drop-target');
+          event.relatedTarget.classList.remove('can-drop');
+        },
+        ondrop: function (event) {
+          //dropzone para la primera respuesta
+          /*if (event.target.id == 'dropzone1' && dropResp1 != null && event.relatedTarget.id != dropResp1) {
+            var id = $('#'+dropResp1).parent()[0].id;
+            $('#'+id).empty();
+            var pos = dropResp1.substr(4,1);
+            $('#'+id).append('<div id="'+dropResp1+'" class="draggable drag-drop" data="'+(gameAt.matrix[pos-1])+'">'+(gameAt.matrix[pos-1])+'</div>');
+            dropResp1 = null; 
+          }
+          if (event.target.id == 'dropzone1'){
+            dropResp1 = event.relatedTarget.id;
+            if (dropResp2 == event.relatedTarget.id)
+              dropResp2 = null;
+          }
+
+          //dropzone para la segunda respuesta
+          if (event.target.id == 'dropzone2' && dropResp2 != null && event.relatedTarget.id != dropResp2) {
+            var id = $('#'+dropResp2).parent()[0].id;
+            $('#'+id).empty();
+            var pos = dropResp2.substr(4,1);
+            $('#'+id).append('<div id="'+dropResp2+'" class="draggable drag-drop" data="'+(gameAt.matrix[pos-1])+'">'+(gameAt.matrix[pos-1])+'</div>');
+            dropResp2 = null; 
+          }
+          if (event.target.id == 'dropzone2'){
+            dropResp2 = event.relatedTarget.id;
+            if (dropResp1 == event.relatedTarget.id)
+              dropResp21 = null;
+          }
+
+          updateMatrix();*/
+        },
+        ondropdeactivate: function (event) {
+          // remove active dropzone feedback
+          event.target.classList.remove('drop-active');
+          event.target.classList.remove('drop-target');
+        }
+      });
+      interact('.drag').draggable({
+        inertia: false,
+        restrict: { restriction: "#content_conocedor", endOnly: true, elementRect: { top: 0, left: 0, bottom: 1, right: 1 } },
+        autoScroll: true,
+        onmove: dragMoveListener,
+        onend: function (event) {
+          /*var bool = false;
+          $(event.target.classList).each(function(index, item){
+            if (item == 'can-drop') {
+              bool = true;
+            }
+          });
+          if(bool == false){
+            if (dropResp1 == event.target.id) {
+              dropResp1 = null;
+            }
+            if (dropResp2 == event.target.id) {
+              dropResp2 = null;
+            }
+          }
+          
+          validateDrag();*/
+        }
+      });
+      window.dragMoveListener = dragMoveListener;
 
     });
 
